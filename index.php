88ed31f9f876e608562c7b73d1b23b4918808718
QDarius <paul.hebert@paulhebertdesigns.com>
2016-04-03 14:23:55 -0700

    Made responsive. Added web fonts

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <!Doctype HTML>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 		<script type="text/javascript" src="js/functions.js"></script>
 		<script type="text/javascript" src="js/thenBy.js"></script>
 
+		<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Open+Sans' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="style.css" />
-
 	</head>
 
 	<body>
--- link.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$txt = file_get_contents('linkedinbloat.txt', FILE_USE_INCLUDE_PATH);
-
-?>
