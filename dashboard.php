d3fc41930f65262a3f1b3b33ba2801c32b2671a5
QDarius <paul.hebert@paulhebertdesigns.com>
2016-07-02 17:09:46 -0700

    Added a third site navigation style for x-large screens. Links are in a fixed sidebar.

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<?php
+	$title = 'The Colors of the Web';
+
+	include('assets/php/header.php');
+?>
+
+	<main>
+		<section>
+			<div class="subsection" id="aggregate">
+				<h1>Dashboard</h1>
+
+				<figure class="large right">
+					<div class="histogram chart hue"></div>
+				</figure>
+
+				<aside class="right">
+					<figure>
+						<svg class="hue fan chart"></svg>
+					</figure>	
+				</aside>
+
+				<figure style="font-size:0px;">
+					<?php
+						$path = 'assets/data/';
+						$files = scandir($path);
+						$array_length = count($files);
+						$files = array_diff($files, array('.', '..'));
+
+						$sites = array_map(function($v){return str_getcsv($v, "|");}, file($path . $files[$array_length-1]));
+
+
+						foreach($sites as $site){
+							for($count = 1; $count < count($site); $count++){
+								echo '<span class="color listing"><span>' . $site[$count] . '</span></span>';
+							}
+						}
+					?>
+				</figure>
+			</div>
+		</section>
+	</main>
+
+<?php
+	include('assets/php/footer.php');
+?>
