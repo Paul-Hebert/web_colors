d9feb699f3d73c3996df65651fb53f916ac3e1ab
QDarius <paul.hebert@paulhebertdesigns.com>
2016-10-02 21:26:14 -0700

    Updated nav layout for medium screens

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,11 +1,3 @@
-	<footer>
-
-		<?php
-			include('assets/php/nav.php');
-		?>
-
-	</footer>
-
 	<?php include('assets/php/scripts.php'); ?>
 
 	</body>
--- assets/php/nav.php
+++ assets/php/nav.php
@@ -5,6 +5,6 @@
 				<a href="https://github.com/Paul-Hebert/web_colors/releases/new">Github</a>
 				<!--<a href="scraper.php">Scraper</a>-->
 				<!--<a href="converter.php">Converter</a>-->
-				<a href="contact.php">Contact</a>
 				<a href="mailing-list.php">Mailing List</a>				
+				<a href="contact.php">Contact</a>
 			</nav>
